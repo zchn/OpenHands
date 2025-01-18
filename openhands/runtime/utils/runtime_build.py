fc70e2f0ae6bed747f80b6e093e29951bf9e9b1a
zchn <kevin.zchn@gmail.com>
2025-01-18 18:14:28 -0500

    Use "pip freeze --all" in get_hash_for_lock_files when 'pyproject.toml' or 'poetry.lock' are not found.

--- openhands/runtime/utils/runtime_build.py
+++ openhands/runtime/utils/runtime_build.py
@@ -309,6 +309,12 @@ def get_hash_for_lock_files(base_image: str):
         src = Path(openhands_source_dir, file)
         if not src.exists():
             src = Path(openhands_source_dir.parent, file)
+        if not src.exists():
+            # TODO(zchn): This is a hack because in experimental mode 
+            # "pip install git+..." won't install pyproject.toml or poetry.lock.
+            # See https://github.com/zchn/OpenHands/blob/0c15d3383d7e0b1c2a16bbd7b54f3cb0a416b219/.github/workflows/openhands-resolver.yml#L215C63-L215C66
+            # and https://github.com/zchn/OpenHands/blob/0c15d3383d7e0b1c2a16bbd7b54f3cb0a416b219/build.sh#L4
+            md5.update(os.popen('pip freeze --all').read())
         with open(src, 'rb') as f:
             for chunk in iter(lambda: f.read(4096), b''):
                 md5.update(chunk)
